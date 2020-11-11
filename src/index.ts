e357aec0f7fba8c286014d0a57fe6c49a454cadc
Andrew Hummel <ahummel25@gmail.com>
2020-11-10 19:58:37 -0600

    Use experimental ts-node esm

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,16 @@ import { getRepo, getRepoFile } from './git';
 const repo = process.argv.slice(2)[0]?.toLowerCase();
 const fileContentsPath = process.argv.slice(2)[1]?.toLowerCase(); // ex. contents/package.json OR contents/some-path-to-file
 
-(async () => {
-	try {
-		const repoResult = await getRepo(repo, fileContentsPath);
+try {
+	const repoResult = await getRepo(repo, fileContentsPath);
 
-		if (fileContentsPath) {
-			const fileResult = await getRepoFile(repoResult.data.download_url);
+	if (fileContentsPath) {
+		const fileResult = await getRepoFile(repoResult.data.download_url);
 
-			console.log(JSON.stringify(fileResult.data, null, 2));
-		} else {
-			console.log(JSON.stringify(repoResult.data, null, 2));
-		}
-	} catch (err) {
-		console.error(err.message);
+		console.log(JSON.stringify(fileResult.data, null, 2));
+	} else {
+		console.log(JSON.stringify(repoResult.data, null, 2));
 	}
-})();
+} catch (err) {
+	console.error(err.message);
+}
